78b6d6df4c773a51dc193a6015de0000af3b123d
roman <roman3668@gmail.com>
2022-11-03 14:42:04 +0300

    Add Task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Enter three-digit number: ");
+int num = int.Parse(Console.ReadLine());
+int numSecond = 0;
+
+if (num > 99 && num < 1000)
+{
+    numSecond = num / 10;
+    numSecond = numSecond % 10;
+    System.Console.WriteLine(numSecond);
+}
+else
+{
+    System.Console.WriteLine("Enter three-digit number");
+}
